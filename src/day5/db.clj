14dfc64d67411b77255298602f0b512337d46241
김형기 <sokoban@gimhyeong-giui-MacBook-Pro.local>
2018-06-11 22:13:38 +0900

    delete db

--- src/day5/db.clj
+++ src/day5/db.clj
@@ -1,6 +1,6 @@
 (ns day5.db
   (:require [conman.core :as conman]))
 
-(def db (conman/connect! {:jdbc-url "jdbc:mysql://sokoban.co.kr:13309/outtest?user=sokoban&password=tnwlEkfkdgo^7&serverTimezone=UTC"}))
+(def db (conman/connect! {:jdbc-url "jdbc:mysql://sokoban.co.kr:/outtest?user=&password=&serverTimezone=UTC"}))
 
 (conman/bind-connection db "query.sql")
